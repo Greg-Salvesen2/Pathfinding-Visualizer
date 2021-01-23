537c568a5cf108795ccee8aa23b19ade526fe69c
Greg <greg@latenightdesigns.com>
2021-01-23 16:10:19 -0500

    Implemented Shortest Path Algorithm

--- src/index.js
+++ src/index.js
@@ -32,6 +32,16 @@ class Grid extends React.Component {
 	}
 }
 
+function Node(x, y, targetX, targetY) {
+	this.distance = Number.MAX_SAFE_INTEGER * 1.0;
+	this.x = x;
+	this.y = y;
+	this.targetX = targetX;
+	this.targetY = targetY;
+	this.visited = false;
+	this.connectingNode = null;
+}
+
 class PathFinder extends React.Component {
 	constructor(props) {
 		super(props);
@@ -43,6 +53,7 @@ class PathFinder extends React.Component {
 			end: false,
 			startText: "Place Start",
 			endText: "Place End",
+			currentNode: new Node(-1, -1, -1, -1),
 		}
 	}
 
@@ -105,15 +116,112 @@ class PathFinder extends React.Component {
 		this.setState({end: !this.state.end});
 	}
 
-	
-	render() {
+	setupNodes() {
+		var nodes = new Array(this.props.height);
+		for(var i = 0; i < nodes.length; i++) {
+			nodes[i] = new Array(this.props.width);
+		}
 
+		const status = this.state.status.slice();
+		let targetX = this.state.endIdx % this.props.width;
+		let targetY = Math.floor(this.state.endIdx / this.props.width)
+		for(i = 0; i < status.length; i++) {
+			let x = i % this.props.width;
+			let y = Math.floor(i / this.props.width);
+			if(status[i] === "wall") {
+				nodes[y][x] = null;
+				continue;
+			}
+			let node = new Node(x, y, targetX, targetY);
+			if(status[i] === "start") {
+				node.distance = 0;
+			} 
+			nodes[y][x] = node;
+		}
+		return nodes;
+	}
+
+	findNextNode(nodes, currentNode) {
+		var smallestX, smallestY;
+		var value = Number.MAX_SAFE_INTEGER;
+		for(var i = 0; i < nodes.length; i++) {
+			for(var j = 0; j < nodes[i].length; j++) {
+				if(nodes[i][j] != null && !nodes[i][j].visited && nodes[i][j].distance < value) {
+					smallestX = j;
+					smallestY = i;
+					value = nodes[i][j].distance;
+				}
+			}
+		}
+		if(value === Number.MAX_SAFE_INTEGER) {
+			return null;
+		} else {
+			return nodes[smallestY][smallestX]
+		}
+	}
+
+	checkSurroundingNodes(nodes, current, diagonal) {
+		var x = current.x;
+		var y = current.y;
+		for(var i = x - 1; i <= x + 1; i++) {
+			for(var j = y - 1; j <= y + 1; j++) {
+				if(i < 0 || i >= this.props.width || j < 0 || j >= this.props.height || nodes[j][i] == null || nodes[j][i].visited) {
+					continue;
+				}
+				if(!diagonal && i !== x && j !== y) {
+					continue;
+				}
+				if(i !== x && j !== y) {
+					if(current.distance + 1.414 < nodes[j][i].distance) {
+						nodes[j][i].distance = current.distance + 1.414;
+						nodes[j][i].connectingNode = current;
+					}
+				} else if(current.distance + 1 < nodes[j][i].distance) {
+					nodes[j][i].distance = current.distance + 1;
+					nodes[j][i].connectingNode = current;
+				}
+
+			}
+		}
+		nodes[current.y][current.x].visited = true;
+		return this.findNextNode(nodes, current);
+	}
+
+	runPathFinding() {
+		if(this.state.startIdx === -1 || this.state.endIdx === -1) {
+			alert("Error: Start and End State must be defined");
+			return;
+		}
+		var nodes = this.setupNodes();
+		let startX = this.state.startIdx % this.props.width;
+		let startY = Math.floor(this.state.startIdx / this.props.width);
+		let targetX = this.state.endIdx % this.props.width;
+		let targetY = Math.floor(this.state.endIdx / this.props.width);
+		let currentNode = nodes[Math.floor(this.state.startIdx / this.props.width)][this.state.startIdx % this.props.width]; 
+		while(currentNode != null && !nodes[targetY][targetX].visited) {
+			currentNode = this.checkSurroundingNodes(nodes, currentNode, true);
+		}
+		if(currentNode != null) {
+			currentNode = nodes[targetY][targetX].connectingNode;
+			const status = this.state.status;
+			while(currentNode !== nodes[startY][startX]) {
+				status[currentNode.y * this.props.width + currentNode.x] = "path";
+				currentNode = currentNode.connectingNode;
+			}
+			this.setState({status: status});
+		} else {
+			alert("No Path to the end found");
+		}
+	}
+
+	render() {
 		return (
 			<div>
 				<Grid width={this.props.width} height={this.props.height} status={this.state.status} handleClick={(x, y) => this.handleClick(x, y)}/>	
 				<div className="bottomBar">
 					<button className={"place"} onClick = {() => this.placeStart()}>{this.state.startText}</button>
 					<button className={"place"} onClick = {() => this.placeEnd()}>{this.state.endText}</button>
+					<button className={"begin"} onClick = {() => this.runPathFinding()}>Run Pathfinder</button>
 				</div>
 			</div>
 		);
